bbf75a7e5804025ee777784fd01c2643180343e4
jrosac <jrosac@me.com>
2024-03-07 08:16:17 -0300

    Added the title in the HTML

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('keydown',function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!audio) return; //stop the function
-    audio.play();
-} );
