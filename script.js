7a5db73a9dca2721693278b9c06a1b8b6efffb41
jrosac <jrosac@me.com>
2024-03-07 08:01:34 -0300

    Added the sounds folder and the sounds file inside it

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener('keydown',function(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if(!audio) return; //stop the function
+    audio.play();
 } );
